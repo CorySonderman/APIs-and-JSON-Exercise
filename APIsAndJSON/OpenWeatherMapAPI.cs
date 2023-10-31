e71d7c141786569fae43d44b2fb464e25ddf6207
CorySonderman <sonderman2000@yahoo.com>
2023-10-31 16:31:26 -0500

    Completed exercise 2

--- APIsAndJSON/OpenWeatherMapAPI.cs
+++ APIsAndJSON/OpenWeatherMapAPI.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace APIsAndJSON
 {
-    internal class OpenWeatherMapAPI
+    public class OpenWeatherMapAPI
     {
+        public static double GetTemp(string apiCall)
+        {
+            var client = new HttpClient();
+            var userInput = client.GetStringAsync(apiCall).Result;
+            var temp = double.Parse(JObject.Parse(userInput)["main"]["temp"].ToString());
+            return temp;
+           
+        }
+        public static string GetCity(string apiCall)
+        {
+            var client = new HttpClient();
+            var userInput = client.GetStringAsync(apiCall).Result;
+            var city = JObject.Parse(userInput)["name"].ToString();
+            return city;
+
+        }
+
     }
 }
--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json.Linq;
+using System.Reflection.Emit;
 using System.Text.Json.Nodes;
 
 namespace APIsAndJSON
@@ -6,19 +7,57 @@ namespace APIsAndJSON
     public class Program
     {
         static void Main(string[] args)
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                RonVSKanyeAPI.KanyeWestQuote();
-                RonVSKanyeAPI.RonSwansonQuote();
-                Console.WriteLine();
-            }
 
-            for (int i = 0; i < 5; i++)
+        {
+            // KanyeVSRon
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    RonVSKanyeAPI.KanyeWestQuote();
+            //    RonVSKanyeAPI.RonSwansonQuote();
+            //    Console.WriteLine();
+            //}
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    RonVSKanyeAPI.RonSwansonQuote();
+            //    RonVSKanyeAPI.KanyeWestQuote();
+            //    Console.WriteLine();
+            //}
+            //_______________________________________________
+
+            //Weather Map
+            var key = File.ReadAllText("appsettings.json");
+            var APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+            string zipCode;
+            do
             {
-                RonVSKanyeAPI.RonSwansonQuote();
-                RonVSKanyeAPI.KanyeWestQuote();
-                Console.WriteLine();
+                Console.WriteLine("Enter you zipcode and I will get the temperature for you. ");
+                zipCode = Console.ReadLine();
+
+                if (zipCode.Length != 5 || !int.TryParse(zipCode, out _))
+                {
+                    Console.WriteLine("That is not a valid zip code. Please enter your 5-digit zip code.");
+                }
+            } while (zipCode.Length != 5 || !int.TryParse(zipCode, out _));
+
+            var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={APIKey}";
+            var temp = OpenWeatherMapAPI.GetTemp(apiCall);
+            var city = OpenWeatherMapAPI.GetCity(apiCall);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            if (temp >= 70)
+            {
+                Console.WriteLine($"It's currently {temp}° in {city}. Looks like t-shirt weather!");
+            }
+            else if (temp >= 40)
+            {
+                Console.WriteLine($"It's currently {temp}° in {city}. It's a little chilly, you might want a jacket or a long-sleeve shirt.");
+            }
+            else
+            {
+                Console.WriteLine($"It's currently {temp}° in {city}. It's pretty cold. Better grab your coat.");
             }
         }
     }

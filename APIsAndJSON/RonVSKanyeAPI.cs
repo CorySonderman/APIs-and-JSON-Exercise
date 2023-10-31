81e1fb947226848b31545795eb54cda61d660282
CorySonderman <sonderman2000@yahoo.com>
2023-10-31 13:06:32 -0500

    Completed Exercise1

--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,31 @@ using System.Threading.Tasks;
 
 namespace APIsAndJSON
 {
-    internal class RonVSKanyeAPI
+    public class RonVSKanyeAPI
     {
+        public static void RonSwansonQuote()
+        {
+            var client = new HttpClient();
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+            ronQuote = ronQuote.Trim('"');
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine($"Ron: {ronQuote}");
+           Console.ResetColor();
+
+        }
+
+        public static void KanyeWestQuote()
+        {
+            var client = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest/";
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").
+                ToString();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Kanye: {kanyeQuote}");
+            Console.ResetColor();
+        }
     }
 }

b7d2aa31f35099d958decee65093c137fdb39feb
CorySonderman <sonderman2000@yahoo.com>
2023-11-01 10:05:10 -0500

    Took out some unnecessary code.

--- APIsAndJSON/MyQuotes.cs
+++ APIsAndJSON/MyQuotes.cs
@@ -8,15 +8,6 @@ namespace APIsAndJSON
 {
     public class MyQuotes
     {
-        private static List<string> cory = new List<string>
-    {
-        "Has anyone ever told you that you're not very bright?",
-        "Well... If life was easy it would be boring.",
-        "The secret to a happy life is easy... Learn to love detours.",
-        "Life isn't fair... Never was, never is, and never will be. It's up to you to either accept it or use it as an excuse.",
-        "You're so inspirational.",
-        "Well my old Sensei used to always tell up. Suck it up butter cup."
-    };
 
         private static List<string> quotes = new List<string>
     {
